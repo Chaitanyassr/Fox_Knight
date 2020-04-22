52d92eed91d543747dd7723452e76ab22827a70c
CHAITANYA <Chaitanyas0101@gmail.com>
2020-04-22 07:07:38 +0530

    add

--- test/Social.js
+++ test/Social.js
@@ -6,10 +6,15 @@ require('chai')
  contract('Social' , (accounts) => {
     let Social
 
-    describe('deployment', () => {
-it('deploys successfully', () => {
+    describe('deployment', async () => {
+it('deploys successfully', async () => {
 
-	
+	Social = await Social.deployed()
+	const address = await Social.address
+	assert.notEqual(address, 0x0)
+	assert.notEqual(address, '')
+	assert.notEqual(address, null)
+	assert.notEqual(address, undefined)
 })
     })
  })
